1994a35cc991a035472b98eb4acb44e0c9b0b5f6
Jerome Toulouse <62750310+DGDGboy@users.noreply.github.com>
2020-04-30 17:58:07 +0200

    Add files via upload

---
+++ abracadabra.js
@@ -0,0 +1,17 @@
+function abracadabra() {
+    
+    let prenom = prompt("Quel est votre prénom ?");
+    let nom    = prompt("Quel est votre nom ?");
+    let age    = prompt("Quel est votre age ?");
+
+    alert("Sapristi ! On ne m'avait pas prévenu que c'était toi, "
+            + prenom
+            + " ! Euh... Je veux dire... La/Le grand(e) "
+            + nom
+            + " ! Cela fait déjà "
+            + age
+            + " ans que tu fais rayonner notre contrée !");
+
+}
+
+abracadabra();
