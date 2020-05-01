8f5f4b66770ac6c59a00e5afdd30237bebb30ee1
Jerome Toulouse <62750310+DGDGboy@users.noreply.github.com>
2020-05-01 15:19:12 +0200

    Update abracadabra.js

--- abracadabra.js
+++ abracadabra.js
@@ -1,16 +1,19 @@
 function abracadabra() {
     
-    let prenom = prompt("Quel est votre prénom ?");
-    let nom    = prompt("Quel est votre nom ?");
-    let age    = prompt("Quel est votre age ?");
+    do {
+    var prenom = prompt("Quel est votre prénom ?");
+    var nom    = prompt("Quel est votre nom ?");
+    var age    = prompt("Quel est votre age ?");
+
+    } while(prenom == "" || prenom == null, nom == "" || prenom == null, age== "" || prenom == null)
 
     alert("Sapristi ! On ne m'avait pas prévenu que c'était toi, "
             + prenom
-            + " ! Euh... Je veux dire... La/Le grand(e) "
+            + " ! Euh... Je veux dire vous... Madame ou Monsieur "
             + nom
             + " ! Cela fait déjà "
             + age
-            + " ans que tu fais rayonner notre contrée ! Bienvenue !!!");
+            + " ans que je vous attends !");
 
 }
 
