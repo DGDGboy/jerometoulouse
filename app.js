8e342a398e0c6e8225293bd133570a1ae662ccbc
Jerome Toulouse <62750310+DGDGboy@users.noreply.github.com>
2021-09-22 14:56:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 
-alert("yo!");
+window.onscroll = function () {
+
+    if (document.documentElement.scrollTop > 100) {
+        document.getElementById("retour").style.background = "black";
+    }
+    else {
+        document.getElementById("retour").style.background = rgb(247, 176, 34);
+    }
+
+
+}
