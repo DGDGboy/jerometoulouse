cd6dcb03de449bdcc4a98911696d216aa386f543
Jerome Toulouse <62750310+DGDGboy@users.noreply.github.com>
2021-09-23 19:37:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ window.onscroll = function () {
 }
 $(function(){
     $("#up").click(function(){
-        $("html, body").animate({scrollTop: 0},"slow");
+        $("html, body").animate({scrollDown: 0},"slow");
     });
 });
